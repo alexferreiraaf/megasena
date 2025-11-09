78c1896fe69f8235325d005c4eaa82746dcf11f9
Alex Ferreira <alexescolpi@gmail.com>
2025-11-09 13:41:25 +0000

    const firebaseConfig = {   apiKey: "AIzaSyC-20CBRga7rCb4_wL-T5FRATHizFx6

---
+++ src/firebase/config.ts
@@ -0,0 +1,14 @@
+// @ts-nocheck
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyC-20CBRga7rCb4_wL-T5FRATHizFx6f7Y",
+  authDomain: "treino-em-casa-g2dqg.firebaseapp.com",
+  projectId: "treino-em-casa-g2dqg",
+  storageBucket: "treino-em-casa-g2dqg.firebasestorage.app",
+  messagingSenderId: "318307654215",
+  appId: "1:318307654215:web:526e4f5f77af8442242f62"
+};
