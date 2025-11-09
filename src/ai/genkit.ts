66b3774b5c5cf882618c7d41998cd423e4c43cb3
Alex Ferreira <alexescolpi@gmail.com>
2025-11-09 13:47:01 +0000

    Error: ./src/ai/genkit.ts:4:1 Export [32mnextPlugin[39m doesn't exist

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,14 +1,13 @@
+'use server';
 import 'dotenv/config';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import {nextPlugin} from '@genkit-ai/next';
 
 export const ai = genkit({
   plugins: [
     googleAI({
       apiKey: process.env.GEMINI_API_KEY,
     }),
-    nextPlugin(),
   ],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
